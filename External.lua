8b5b7324275b19b98b1a325db1bf4ff7e34ac900
nil <143337445+LuaU0@users.noreply.github.com>
2023-08-27 17:54:20 +0300

    Update External.lua

--- External.lua
+++ External.lua
@@ -2,3 +2,5 @@ local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
+--// Request 
+local request = (syn and syn.request) or (http and http.request) or http_request
