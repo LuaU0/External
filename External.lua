cdd650c76236b00f2cc9073f389fe2668a2929e6
nil <143337445+LuaU0@users.noreply.github.com>
2023-08-27 17:52:17 +0300

    Create External.lua

---
+++ External.lua
@@ -0,0 +1,4 @@
+local UserInputService = game:GetService("UserInputService")
+local TweenService = game:GetService("TweenService")
+local HttpService = game:GetService("HttpService")
+local RunService = game:GetService("RunService")
